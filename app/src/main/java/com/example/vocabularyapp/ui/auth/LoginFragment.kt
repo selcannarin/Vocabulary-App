a2242e25aa492fc9c9a9b3118f0184618f85a128
Yencansu5lerr <cansu5ler@gmail.com>
2023-12-24 23:26:22 +0300

    add backPressed fun

--- app/src/main/java/com/example/vocabularyapp/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/vocabularyapp/ui/auth/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
@@ -29,6 +30,7 @@ class LoginFragment : Fragment() {
     ): View? {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
 
+        onBackPressed()
         setupListeners()
         handleEvents()
         handleErrors()
@@ -104,6 +106,12 @@ class LoginFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    private fun onBackPressed() {
+        requireActivity().onBackPressedDispatcher.addCallback(this) {
+            requireActivity().finish()
+        }
     }
 
 }
